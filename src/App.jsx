96fcb01f67c9b331e4ce5c1024c0a9a5cf526049
Esteban Camilo Urbano <esteban.urbano@correounivalle.edu.co>
2025-10-20 10:53:53 -0700

    Progreso antes de fusionar con main

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
+import Navbar from "./Navbar";
+
 const App = () => {
   return (
-    <div className="login-container">
-      <h2 className="form-title">log in with</h2>
+    <div>
+      <Navbar />
+      <main className="p-4">
+        <h2 className="text-2xl font-semibold text-gray-800">Dashboard</h2>
+        <p className="text-gray-500 mt-2">..................</p>
+      </main>
     </div>
   );
 };
 
-export default App;
+export default App
