2730cc723062d6b3b371acf4fd98ab9a0769778b
Daniel Enriquez <danicraft.op7@gmail.com>
2025-10-21 08:10:49 -0500

    configure login and user example for data base in mongo atlas

--- backend/renai-project/src/app.module.ts
+++ backend/renai-project/src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/renaiDB'),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    MongooseModule.forRoot(process.env.MONGO_URI!),
     AuthModule,
   ],
 })
