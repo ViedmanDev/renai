af467454439fb9395e1ae872e452740e5184973f
Daniel Enriquez <danicraft.op7@gmail.com>
2025-11-09 13:12:25 -0500

    add modules and services to connect with database

--- backend/renai-project/src/app.module.ts
+++ backend/renai-project/src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
+import { ProjectsModule } from './projects/projects.module';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { AuthModule } from './auth/auth.module';
     }),
     MongooseModule.forRoot(process.env.MONGO_URI!),
     AuthModule,
+    ProjectsModule,
   ],
 })
 export class AppModule {}
