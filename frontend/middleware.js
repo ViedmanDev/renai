48ee295a08545bb3713439a8454971245fcecabc
DaniJER <danicraft.op7@gmail.com>
2025-12-02 02:05:36 -0500

    add middleware for cors

---
+++ frontend/middleware.js
@@ -0,0 +1,25 @@
+import { NextResponse } from "next/server";
+
+export function middleware(req) {
+  const res = NextResponse.next();
+
+  res.headers.set("Access-Control-Allow-Origin", "*");
+  res.headers.set(
+    "Access-Control-Allow-Methods",
+    "GET,POST,PUT,PATCH,DELETE,OPTIONS"
+  );
+  res.headers.set(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization"
+  );
+
+  if (req.method === "OPTIONS") {
+    return new NextResponse(null, { status: 200, headers: res.headers });
+  }
+
+  return res;
+}
+
+export const config = {
+  matcher: ["/auth/:path*"],
+};
